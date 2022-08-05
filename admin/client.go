095da685fd0f355b30b0156015b7ff1d9cc0d9a4
yusank <yusankurban@gmail.com>
2022-08-05 11:30:07 +0800

    feat: update admin client

--- admin/client.go
+++ admin/client.go
@@ -3,7 +3,6 @@ package admin
 import (
 	"context"
 
-	"github.com/tsuna/gohbase/hrpc"
 	"github.com/tsuna/gohbase/pb"
 )
 
@@ -19,21 +18,28 @@ type Client interface {
 	SnapshotType(*pb.SnapshotDescription_Type) Client
 	SnapshotVersion(int32) Client
 	SnapshotOwner(string) Client
+	// list
+	Regex(string) Client
+	IncludeSysTables(bool) Client
+	Namespace(string) Client
+	// move region
+	TargetRegion(string) Client
+	TargetServerName(string) Client
 
 	// operations
 
-	CreateTable(t *hrpc.CreateTable) Result
-	DeleteTable(t *hrpc.DeleteTable) Result
-	EnableTable(t *hrpc.EnableTable) Result
-	DisableTable(t *hrpc.DisableTable) Result
-	CreateSnapshot(t *hrpc.Snapshot) Result
-	DeleteSnapshot(t *hrpc.Snapshot) Result
-	ListSnapshots(t *hrpc.ListSnapshots) Result
-	RestoreSnapshot(t *hrpc.Snapshot) Result
+	CreateTable() Result
+	DeleteTable() Result
+	EnableTable() Result
+	DisableTable() Result
+	CreateSnapshot() Result
+	DeleteSnapshot() Result
+	ListSnapshots() Result
+	RestoreSnapshot() Result
 	ClusterStatus() Result
-	ListTableNames(t *hrpc.ListTableNames) Result
+	ListTableNames() Result
 	// SetBalancer sets balancer state and returns previous state
-	SetBalancer(sb *hrpc.SetBalancer) Result
+	SetBalancer(enabled bool) Result
 	// MoveRegion moves a region to a different RegionServer
-	MoveRegion(mr *hrpc.MoveRegion) Result
+	MoveRegion() Result
 }
